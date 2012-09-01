e16875964ad519b00bd3ed841c88e2e682ae00e2
Henry Lawson <hlawson@thoughtworks.com>
2012-09-02 01:29:12 +0530

    Moved to global asax

--- Source/Blog/Global.asax.cs
+++ Source/Blog/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using System.Web.Routing;
 using Blog.App_Start;
 using Blog.Dependencies;
@@ -7,12 +8,25 @@ namespace Blog
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public MvcApplication()
+        {
+            EndRequest += OnEndRequest;
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             StructureMapSetup.InitializeContainer();
+        }
+
+        private void OnEndRequest(object sender, EventArgs eventArgs)
+        {
+            if (Context.Response.StatusCode == 401)
+            {
+                Context.Response.RedirectToRoute("Error404");
+            }
         }
     }
 }
